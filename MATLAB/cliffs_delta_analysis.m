eee02fb0b3fae6fabaf3c1946f2691a2a6f57751
simonsprigode-jpg <simon.sprigode@gmail.com>
2026-08-10 15:38:31 +0200

    Update cliffs_delta_analysis.m

--- MATLAB/cliffs_delta_analysis.m
+++ MATLAB/cliffs_delta_analysis.m
@@ -10,13 +10,17 @@ clear; clc; close all;
 
 %% 1) Daten- und Ausgabeordner
 
-dataDir = 'C:\Users\simon\BA Ahrtal 2026';
+dataDir = uigetdir(pwd, 'Ordner mit den Eingangsdaten auswählen');
+
+if isequal(dataDir, 0)
+    error('Kein Datenordner ausgewählt.');
+end
+
 outDir = fullfile(dataDir, 'plots_cliffs_delta');
 
 if ~exist(outDir, 'dir')
     mkdir(outDir);
 end
-
 
 %% 2) Eingabedateien
 
